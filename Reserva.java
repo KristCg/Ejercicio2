17817e6305e516dcf339cd4429e39c1dc89e295f
KristCg <kriscastillo515@gmail.com>
2024-08-28 19:43:36 -0600

    Reserva clase

---
+++ Reserva.java
@@ -0,0 +1,44 @@
+package Ejercicio2;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Reserva{
+    private String nombre;
+    private String encargado;
+    private int salonAsignado;
+    private Date fecha;
+    private Time horario;
+
+    public Reserva(String nombre, String encargado, int salonAsignado, Date fecha, Time horario){
+        this.nombre = nombre;
+        this.encargado = encargado;
+        this.salonAsignado = salonAsignado;
+        this.fecha = fecha;
+        this.horario = horario;    
+    }
+    public String getNombre(){
+        return nombre; 
+    }
+    public String getEncargado(){
+        return encargado;
+    }
+    public int getSalonAsignado(){
+        return salonAsignado;
+    }
+    public Date getFecha(){
+        return fecha;
+    }
+    public Time getHorario(){
+        return horario;
+    }
+    public boolean asignarSalon(int salon){
+        if(salonAsignado == 0){
+            salonAsignado = 0;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }    
+}
